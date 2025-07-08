48bd3059576e91db913d3be2fa7de336e0e8d17d
Gerson <dev@geniuscode.io>
2025-07-08 14:30:59 +1000

    Remove node_modules from base theme

--- templates/resources/node_modules/hoverintent/ender.js
+++
@@ -1,9 +0,0 @@
-!function($) {
-  $.ender({
-    hoverintent: function(over, out) {
-      return this.forEach(function(el) {
-        hoverintent(el, over, out).options(opt || {});
-      });
-    }
-  }, true);
-}(ender);
